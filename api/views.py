8fcf3f74d99166092a5a1049cf2a7ca89061593c
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2017-03-04 08:38:06 +0600

    argmax on pred

--- api/views.py
+++ api/views.py
@@ -57,7 +57,7 @@ class PhotoList(APIView):
             result = get_pred(full_filename)
             veg_index = np.argmax(result)
 
-            return Response({'key': result, veg_index=veg_index}, status=status.HTTP_201_CREATED)
+            return Response({'key': result, veg_index:veg_index}, status=status.HTTP_201_CREATED)
         except Exception as inst:
             raise inst
             return Response({'key': 'NOT SAVED'}, status=status.HTTP_201_CREATED)
